888c60c9e1c57f308c4d7afb5e6e3959f3be4e1c
DE-TOX <djknight0140@gmail.com>
2024-02-22 17:30:10 +0530

    [DIVYANSH] Modified: Validation added

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,7 +1,63 @@
 import './SignUp.css';
 import signupLogo from '../../images/logo-google.png'
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+
+
+
+
+
 function SignUp() {
+    const [userdetails, setDetails] = useState({ fname: "", lname: "", uname: "", pass: "", cpass: "" })
+    const [validations, setValidation] = useState({ fname: true, lname: true, uname: true, pass: true, cpass: true })
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setDetails(prevDetails => ({
+            ...prevDetails,
+            [name]: value
+        }));
+        validateInput(name, value)
+    };
+
+    const validateInput = (name, value) => {
+        let isValid = true;
+        let regex;
+    
+        switch (name) {
+            case 'fname':
+            case 'lname':
+                regex = /^[A-Za-z]+$/;
+                isValid = regex.test(value.trim());
+                break;
+            case 'uname':
+                isValid = value.trim().length >= 3;
+                break;
+            case 'pass':
+                isValid = value.length >= 8;
+                break;
+            case 'cpass':
+                
+                isValid = value === userdetails.pass;
+                break;
+            default:
+                break;
+        }
+        setValidation(prevValidation => ({
+            ...prevValidation,
+            [name]: isValid
+        }));
+    };
+    const handleClick = () => {
+        const isValid = Object.values(validations).every(v => v);
+        if (!isValid) {
+            alert('Please correct the errors in the form.');
+            return;
+        }
+
+        console.log(userdetails)
+    }
+
+
     return (
         <div className='signUp'>
             <div className='container'>
@@ -11,25 +67,25 @@ function SignUp() {
                         <h2>Create your Google Account</h2>
                     </div>
                     <div className="inputs">
-                        <input type="text" name="" id="text" className="input" placeholder='First Name*' />
-                        <input type="text" name="" id="text" className="input" placeholder='Last Name*' />
+                        <input type="text" name="fname" id="text" className="input" placeholder='First Name*' onChange={handleChange} />
+                        <input type="text" name="lname" id="text" className="input" placeholder='Last Name*' onChange={handleChange} />
                         <div className='username'>
-                            <input type="text" name="" id="text-username" className="input" placeholder='username*' />
+                            <input type="text" name="uname" id="text-username" className="input" placeholder='username*' onChange={handleChange} />
                             <label style={{ fontSize: "0.7em" }}>you can use letters, numbers & periods</label>
                             <br />
                             <p className='p1'>Use my current email instead</p>
                             <br />
                         </div>
-                        <input type="text" name="" id="text" className="input" placeholder='Password*' />
-                        <input type="text" name="" id="text" className="input" placeholder='Confirm*' />
+                        <input type="text" name="pass" id="text" className="input" placeholder='Password*' onChange={handleChange} />
+                        <input type="text" name="cpass" id="text" className="input" placeholder='Confirm*' />
                         <p className='p1'>Use 8 or more characters with a mix of letters, numbers & symbols</p>
                         <div className='checkbox'>
                             <input type="checkbox" /><p >Show Password</p>
                         </div>
                     </div>
                     <div class="btn-group">
-                        <button class="create-btn"><a href = "/login">Sign in insted</a></button>
-                        <button class="next-btn">Next</button>
+                        <button class="create-btn"><a href="/login">Sign in insted</a></button>
+                        <button class="next-btn" onClick={handleClick}>Next</button>
 
                     </div>
                 </div>
